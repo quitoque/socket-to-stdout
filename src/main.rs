d155a16477fc164b945b7ea3190da335e78383ae
Jerome Arzel <jerome@quitoque.fr>
2018-11-24 14:15:40 +0100

    Honor MIT

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,4 @@
-//! A "hello world" echo server with Tokio
-//!
-//! This server will create a TCP listener, accept connections in a loop, and
-//! write back everything that's read off of each TCP connection.
-//!
-//! Because the Tokio runtime uses a thread pool, each TCP connection is
-//! processed concurrently with all other TCP connections across multiple
-//! threads.
-//!
-//! To see this server in action, you can run this in one terminal:
-//!
-//!     cargo run --example echo
-//!
-//! and in another terminal you can run:
-//!
-//!     cargo run --example connect 127.0.0.1:8080
-//!
-//! Each line you type in to the `connect` terminal should be echo'd back to
-//! you! If you open up multiple terminals running the `connect` example you
-//! should be able to see them all make progress simultaneously.
+//! This code is base on the Tokio echo server example
 
 #![deny(warnings)]
 
